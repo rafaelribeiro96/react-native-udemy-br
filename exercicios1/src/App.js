c8f37e4d70ffdf6a439de99607618e454bfe3570
rafaelribeiro96 <rafaelfelipe.r@hotmail.com>
2023-03-28 12:29:16 -0300

    continuando estudos com react native, passando por questão de props e fragment

--- exercicios1/src/App.js
+++ exercicios1/src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
+import Title from './components/title';
 
 /* import CompPadrao, {Comp1, Comp2} from './components/Multi';
 import Primeiro from './components/Primeiro'; */
-import MinMax from './components/MinMax';
-import NumAleatorio from './components/NumAleatorio';
+/* import MinMax from './components/MinMax';
+import NumAleatorio from './components/NumAleatorio'; */
 
 function App() {
   return (
     <View style={style.App}>
+      <Title
+        principal="Cadastro Produto"
+        secundario="Tela de Cadastro do Produto"
+      />
       {/* <CompPadrao />
       <Comp1 />
       <Comp2 />
-      <Primeiro /> */}
+      <Primeiro />
       <MinMax min={3} max={20} />
-      <NumAleatorio num1={1} num2={1000} />
+      <NumAleatorio num1={1} num2={1000} /> */}
     </View>
   );
 }
