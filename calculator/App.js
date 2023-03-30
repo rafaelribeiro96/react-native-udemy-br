fadcd54b1e68a7b366e3b548f015c6187ab24806
rafaelribeiro96 <rafaelfelipe.r@hotmail.com>
2023-03-30 12:35:07 -0300

    criado components da calculadora e criado style dos elementos

--- calculator/App.js
+++ calculator/App.js
@@ -1,12 +1,46 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import Button from './src/components/Button';
+import Display from './src/components/Display';
 
 export default class App extends Component {
+  state = {
+    displayValue: '0',
+  };
+
+  addDigit = n => {
+    this.setState({displayValue: n});
+  };
+
+  clearMemory = () => {
+    this.setState({displayValue: '0'});
+  };
+
+  setOperation = operation => {};
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome</Text>
-        <Text style={styles.instructions}>App.js</Text>
+        <Display value={this.state.displayValue} />
+        <View style={styles.buttons}>
+          <Button label="AC" triple onClick={this.clearMemory} />
+          <Button label="/" operation onClick={this.setOperation} />
+          <Button label="7" onClick={this.addDigit} />
+          <Button label="8" onClick={this.addDigit} />
+          <Button label="9" onClick={this.addDigit} />
+          <Button label="*" operation onClick={this.setOperation} />
+          <Button label="4" onClick={this.addDigit} />
+          <Button label="5" onClick={this.addDigit} />
+          <Button label="6" onClick={this.addDigit} />
+          <Button label="-" operation onClick={this.setOperation} />
+          <Button label="1" onClick={this.addDigit} />
+          <Button label="2" onClick={this.addDigit} />
+          <Button label="3" onClick={this.addDigit} />
+          <Button label="+" operation onClick={this.setOperation} />
+          <Button label="0" double onClick={this.addDigit} />
+          <Button label="." onClick={this.addDigit} />
+          <Button label="=" operation onClick={this.setOperation} />
+        </View>
       </View>
     );
   }
@@ -15,18 +49,9 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  buttons: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
   },
 });
