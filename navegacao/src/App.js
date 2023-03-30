6119b85d0a9c73987c28607b6a53031a7c4e73e7
rafaelribeiro96 <rafaelfelipe.r@hotmail.com>
2023-03-30 18:52:32 -0300

    configuração de ambiente

--- navegacao/src/App.js
+++ navegacao/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
-import TextoCentral from './components/TextoCentral';
+import {SafeAreaView} from 'react-native';
+import TelaA from './views/TelaA';
+import TelaB from './views/TelaB';
+import TelaC from './views/TelaC';
 
 export default props => {
   return (
     <SafeAreaView style={{flex: 1}}>
-      <TextoCentral>App</TextoCentral>
+      <TelaA />
+      <TelaB />
+      <TelaC />
     </SafeAreaView>
   );
 };
---
+++ navegacao/src/views/TelaC.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import TextoCentral from '../components/TextoCentral';
+
+export default props => {
+  return <TextoCentral corFundo="#9932cd">Tela C</TextoCentral>;
+};
