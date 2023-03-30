898a16a3c02887339b364fa73fecb99be10a540b
rafaelribeiro96 <rafaelfelipe.r@hotmail.com>
2023-03-30 17:18:43 -0300

    Inicio projeto navegacao em react native

---
+++ navegacao/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+import TextoCentral from './components/TextoCentral';
+
+export default props => {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <TextoCentral>App</TextoCentral>
+    </SafeAreaView>
+  );
+};
