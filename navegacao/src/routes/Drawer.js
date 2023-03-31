e1a61a8f6a5e37fffa7a8d7d6c411207af43f7d8
rafaelribeiro96 <rafaelfelipe.r@hotmail.com>
2023-03-31 12:36:37 -0300

    praticando navegação drawer

---
+++ navegacao/src/routes/Drawer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import TelaA from '../views/TelaA';
+import TelaB from '../views/TelaB';
+import TelaC from '../views/TelaC';
+
+const Drawer = createDrawerNavigator();
+
+export default props => (
+  <Drawer.Navigator
+    tabBatOptions={{
+      activeTintColor: 'red',
+      inactiveTintColor: 'blue',
+      labelStyle: {fontSize: 30},
+    }}
+    initialRouteName="TelaA">
+    <Drawer.Screen name="TelaA" component={TelaA} />
+    <Drawer.Screen name="TelaB" component={TelaB} />
+    <Drawer.Screen name="TelaC" component={TelaC} />
+  </Drawer.Navigator>
+);
